51eab95fc202f96a6ade5841cb754b45f685ed17
susueii <78355613+susueii@users.noreply.github.com>
2021-02-02 12:57:14 +0800

    Update project.js

--- src/project.js
+++ src/project.js
@@ -1154,7 +1154,7 @@ window.__require = function e(t, n, o) {
                     var t = this,
                         n = cc.instantiate(this.fruitPre);
                     n.parent = this.lineNode, n.getComponent(cc.Sprite).spriteFrame = d.default.Instance.fruit[e], n.children[0].getComponent(cc.Sprite).spriteFrame = d.default.Instance.fruit[e], n.getComponent("fruitData").fruitNumber = e, n.position = this.lineNode.children[1].position, n.scale = 0, n.getComponent(cc.RigidBody).type = cc.RigidBodyType.Static, n.getComponent(cc.PhysicsCircleCollider).radius = 0, 
-					n.getComponent(cc.PhysicsCircleCollider).restitution = 0.9,
+					n.getComponent(cc.PhysicsCircleCollider).restitution = 0.5,
 					n.getComponent(cc.PhysicsCircleCollider).apply(), cc.tween(n).to(.5, {
                         scale: 1
                     }, {
